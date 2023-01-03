b04792a927bc67e7fe0435f01c3a30401ddc848d
Daniel Steinberg <daniel@dimsumthinking.com>
2023-01-03 10:43:31 -0500

    Up through Ch 4 3 draft

--- Chapter04/03/Structure/Structure/Controllers/EntryController.swift
+++ Chapter04/03/Structure/Structure/Controllers/EntryController.swift
@@ -8,25 +8,33 @@ class EntryController: ObservableObject {
   
   private let plain = NumberVendor(delay: 2.0)
   private let filled = NumberVendor(delay: 1.5)
+  
+  private var nextTask: Task<Void, Error>?
 }
 
 extension EntryController {
   func nextPair() {
     clear()
-    Task {
-      async let plainNumber = plain.randomNumber()
-      async let filledNumber = filled.randomNumber()
-      comparison = Comparison(await plainNumber,
-                              await filledNumber)
-      plainEntry = Entry(number: await plainNumber)
-      filledEntry = Entry(number: await filledNumber,
-                          isFilled: true)
+    nextTask = Task {
+      do {
+        async let plainNumber = plain.randomNumber()
+        async let filledNumber = filled.randomNumber()
+        comparison = Comparison(try await plainNumber,
+                                try await filledNumber)
+        if Task.isCancelled { print("nextTask is cancelled")}
+        plainEntry = Entry(number: try await plainNumber)
+        filledEntry = Entry(number: try await filledNumber,
+                            isFilled: true)
+      } catch {
+        print(error)
+      }
     }
   }
 }
 
 extension EntryController {
   private func clear() {
+    nextTask?.cancel()
     plainEntry = nil
     filledEntry = nil
     comparison = nil
--- Chapter04/03/Structure/Structure/Controllers/NumberVendor.swift
+++ Chapter04/03/Structure/Structure/Controllers/NumberVendor.swift
@@ -1,8 +1,8 @@
 struct NumberVendor {
   let delay: Double
   
-  func randomNumber() async -> Int {
-    try? await Task.sleep(for: .seconds(delay))
+  func randomNumber() async throws -> Int {
+    try await Task.sleep(for: .seconds(delay))
     return Int.random(in: 1...50)
   }
 }
